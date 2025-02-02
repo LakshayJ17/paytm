645185ace203088bf2477acca7fccebc98c655c7
lakshay17 <lakshayjain01lj@gmail.com>
2025-02-02 16:45:18 +0530

    add JWT secret configuration, enable CORS, and set up user routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const mainRouter = require("./routes/index")
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+
+const mainRouter = require("./routes/index")
 const app = express();
 
-// When a request comes from /api/v1, it will be redirected to mainRouter
+// Any request to /api/v1 path is forwarded to the mainRouter
+// This creates a base URL structure for your API (e.g., http://yourserver.com/api/v1/...)
 app.use("/api/v1", mainRouter)
+
+app.listen(3000)
---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+// Sets up specific routes for user-related operations
+// eg - /api/v1/user/signup
+
+const express = require("express")
+
+const router = express.Router()
+
+router.post("/",(req,res)=>{
+    
+})
+
+module.exports = router
